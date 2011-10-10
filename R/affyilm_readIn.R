e3eb66b3daea4385a3f4aa0333f69bb199f27063
Fabrice Berger <fabrice.berger@fys.kuleuven.be>
2011-10-10 10:14:39 +0000

    Removed background estimation, replaced parts of C code with R code, and cleaned the code.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyILM@58955 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/affyilm_readIn.R
@@ -0,0 +1,39 @@
+.getChipInfo <- function(chiptype) {
+
+## Following few lines adapted from gcrma-package
+# Load library, download automatically if not installed 
+	cdf.tag <- cleancdfname(chiptype,addcdf=FALSE)
+	probepackagename <- paste(cdf.tag,"probe",sep="")
+	getProbePackage(probepackagename)
+	cat(paste("Probepackage", probepackagename,"loaded \n"),sep="")
+	probetype <- get(probepackagename)
+
+### In case cdf-package is needed... ###
+#	cdfpackagename <- paste(cdf.tag,"cdf",sep="") 
+#	getCDF(cdfpackagename)
+
+	return(list(probetype,probepackagename))
+
+}
+
+
+.identifyChip <- function(celfiles) {
+	headers <- lapply(celfiles, readCelHeader)
+	chiptype <- unique(sapply(headers, "[[", i="chiptype"))
+
+	if(length(chiptype)!=1){
+		stop("Different chiptypes!")
+	} else {
+		ncols <- headers[[1]]$cols
+		nrows <- headers[[1]]$rows
+		if(all.equal(ncols,nrows)){
+			chipdim <- ncols
+			cat(paste("Chip dimension",chipdim," x ",chipdim, "\n"),sep="")
+		} else {
+			stop("Sorry, in the current version of 
+affyILM, only chips with ncol=nrow can be analyzed...")
+		}
+	}
+
+	return(list(chiptype,chipdim,ncols,nrows))
+}
